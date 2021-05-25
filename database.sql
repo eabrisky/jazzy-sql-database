e8157bcd0d19bf174e24c961df1692132e1fcc8e
Emry Brisky <eabrisky@gmail.com>
2021-05-25 17:26:19 -0500

    initial commit

--- database.sql
+++ database.sql
@@ -1,5 +1,35 @@
-CREATE TABLE "artists" (
+CREATE TABLE "artist" (
     "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+    "name" varchar(80) not null,
+    "birthdate" date
 );
+
+CREATE TABLE "song" (
+    "id" SERIAL PRIMARY KEY,
+    "title" varchar(255) not null,
+    "length" varchar(10),
+    "released" date
+);
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Ella Fitzgerald', '04-25-1917');
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Dave Brubeck', '12-06-1920');
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Miles Davis', '05-26-1926');
+
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Esperanza Spalding', '10-18-1984');
+
+
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('Take Five', '5:24', '1959-09-29');
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('So What', '9:22', '1959-08-17');
+
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('Black Gold', '5:17', '2012-02-01');
--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg = require('pg');
+const Pool = pg.Pool;
 
 const app = express();
 const PORT = 5000;
+
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'localhost',
+    port: 5432
+});
+
+pool.on('connect', () => {
+    console.log('connected to postgres');
+});
+
+pool.on('error', (error) => {
+    console.log(error);
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
@@ -12,50 +28,72 @@ app.listen(PORT, () => {
 });
 
 // TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
 
 app.get('/artist', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(artistList);
+
+    const queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`
+    pool.query(queryText)
+    .then((result) => {
+        console.log(result.rows);
+        res.send(result.rows);  
+    }) // end .then pool query
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    }); // end .catch pool query
 });
 
 app.post('/artist', (req, res) => {
-    artistList.push(req.body);
-    res.sendStatus(201);
+
+    console.log('req.body', req.body);
+    
+    let queryText = `INSERT INTO "artist" ("name", "birthdate")
+    VALUES ($1, $2,);`
+
+    let values = [req.body.name, req.body.birthdate];
+
+    pool.query(queryText, values)
+    .then ( (result) => {
+        res.sendStatus(201);
+    }) // end .then
+    .catch( err => {
+        res.sendStatus(500);
+    }) // end .catch
 });
 
 app.get('/song', (req, res) => {
