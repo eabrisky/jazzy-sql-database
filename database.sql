958cf12b07208c10f6915116693b54f15c5b72a1
Emry Brisky <eabrisky@gmail.com>
2021-05-25 22:45:24 -0500

    updated database.sql to get rid of manually entered ids for each song and artist insert

--- database.sql
+++ database.sql
@@ -11,25 +11,25 @@ CREATE TABLE "song" (
     "released" date
 );
 
-INSERT INTO "artist" ("id", "name", "birthdate")
-VALUES ('1', 'Ella Fitzgerald', '04-25-1917');
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Ella Fitzgerald', '04-25-1917');
 
-INSERT INTO "artist" ("id", "name", "birthdate")
-VALUES ('2', 'Dave Brubeck', '12-06-1920');
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Dave Brubeck', '12-06-1920');
 
-INSERT INTO "artist" ("id", "name", "birthdate")
-VALUES ('3', 'Miles Davis', '05-26-1926');
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Miles Davis', '05-26-1926');
 
-INSERT INTO "artist" ("id", "name", "birthdate")
-VALUES ('4', 'Esperanza Spalding', '10-18-1984');
+INSERT INTO "artist" ("name", "birthdate")
+VALUES ('Esperanza Spalding', '10-18-1984');
 
 
 
-INSERT INTO "song" ("id", "title", "length", "released")
-VALUES ('5', 'Take Five', '5:24', '1959-09-29');
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('Take Five', '5:24', '1959-09-29');
 
-INSERT INTO "song" ("id", "title", "length", "released")
-VALUES ('6', 'So What', '9:22', '1959-08-17');
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('So What', '9:22', '1959-08-17');
 
-INSERT INTO "song" ("id", "title", "length", "released")
-VALUES ('7', 'Black Gold', '5:17', '2012-02-01');
+INSERT INTO "song" ("title", "length", "released")
+VALUES ('Black Gold', '5:17', '2012-02-01');
